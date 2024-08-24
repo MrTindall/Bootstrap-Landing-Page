8f6166e3063afb2fc010d2ab53f9946a06ceb1cf
Alex <tindall.al@gmail.com>
2024-08-24 11:30:31 -0500

    added cards with count up and icons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const percent = document.getElementById("percent");
+    const cost = document.getElementById("cost");
+    const salary = document.getElementById("salary")
+
+
+    countToo(96, percent, 1, 25);
+    countToo(149, cost, 1, 20);
+    countToo(51960, salary, 100, 1)
+
+
+    function countToo(upto, elementId, countAmount, time) {
+        let current = parseInt(elementId.innerHTML, 10) || 0;
     
+        const interval = setInterval(() => {
+            if (current < upto) {
+                current += countAmount;
+                elementId.innerHTML = current;
+            } else {
+                clearInterval(interval);
+            }
+        }, time);
+    }
 })
