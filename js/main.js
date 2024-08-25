07334a9660b2c6ee310e8d5bca7cf4d78c4502b3
Alex <tindall.al@gmail.com>
2024-08-25 08:42:31 -0500

    refactored js

--- js/main.js
+++ js/main.js
@@ -4,21 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const salary = document.getElementById("salary")
 
 
-    countToo(96, percent, 1, 25);
-    countToo(149, cost, 1, 20);
-    countToo(51960, salary, 100, 1)
+    countUpTo(96, percent, 1, 25);
+    countUpTo(149, cost, 1, 20);
+    countUpTo(51960, salary, 60, 1)
 
-
-    function countToo(upto, elementId, countAmount, time) {
-        let current = parseInt(elementId.innerHTML, 10) || 0;
-    
-        const interval = setInterval(() => {
-            if (current < upto) {
-                current += countAmount;
-                elementId.innerHTML = current;
-            } else {
-                clearInterval(interval);
-            }
-        }, time);
-    }
 })
+
+function countUpTo(upto, elementId, countAmount, time) {
+    let current = parseInt(elementId.innerHTML, 10) || 0;
+
+    const interval = setInterval(() => {
+        if (current < upto) {
+            current += countAmount;
+            elementId.innerHTML = current;
+        } else {
+            clearInterval(interval);
+        }
+    }, time);
+}
